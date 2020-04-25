7c740c3fe3c6f7427f59a6c700ca1fda05c452f8
0xDiego <31803752+0xDiego@users.noreply.github.com>
2020-04-25 11:36:54 -0300

    Add files via upload

---
+++ Scripts/ParseoTorneos.js
@@ -0,0 +1,6 @@
+var xmlhttp, xmlDoc;
+xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "texto.txt", false);
+xmlhttp.send();
+xmlDoc = xmlhttp.response;
+document.getElementById("contTrnRes").innerHTML = "hola"; //xmlDoc;
